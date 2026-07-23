44b8527de862286e97b5ada5158f30bcee80ed27
woofish <921982125@qq.com>
2026-07-23 17:18:43 +0800

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.alanbulan.tunefree',
-  appName: 'tunefree-mobile',
-  webDir: 'dist'
-};
-
-export default config;
